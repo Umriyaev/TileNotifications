c00257cb67b6fbf1b77a53f0002370ba6f711762
Shokhrukh <kinglerrr@hotmail.com>
2015-01-30 18:00:54 -0800

    Added logic for client and server

--- Client app/MainPage.xaml.cs
+++ Client app/MainPage.xaml.cs
@@ -7,12 +7,15 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Notification;
+using System.Diagnostics;
 using Client_app.Resources;
 
 namespace Client_app
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        HttpNotificationChannel notificationChannel;
         // Constructor
         public MainPage()
         {
@@ -20,6 +23,110 @@ namespace Client_app
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+        }
+
+        Uri channelUri;
+        public Uri ChannelUri
+        {
+            get { return this.channelUri; }
+            set
+            {
+                this.channelUri = value;
+                Debug.WriteLine(value.ToString());
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            SetupChannel();
+        }
+
+        private void SetupChannel()
+        {
+            string channelName = "Demo notification channel";
+            notificationChannel = HttpNotificationChannel.Find(channelName);
+            if (notificationChannel != null)
+            {
+                notificationChannel.ChannelUriUpdated += notificationChannel_ChannelUriUpdated;
+                notificationChannel.ErrorOccurred += notificationChannel_ErrorOccurred;
+                notificationChannel.HttpNotificationReceived += notificationChannel_HttpNotificationReceived;
+                notificationChannel.ShellToastNotificationReceived += notificationChannel_ShellToastNotificationReceived;
+                notificationChannel.ConnectionStatusChanged += notificationChannel_ConnectionStatusChanged;
+                Debug.WriteLine(notificationChannel.ChannelUri.ToString());
+            }
+            else
+            {
+                notificationChannel = new HttpNotificationChannel(channelName);
+
+                notificationChannel.ChannelUriUpdated += notificationChannel_ChannelUriUpdated;
+                notificationChannel.ErrorOccurred += notificationChannel_ErrorOccurred;
+                notificationChannel.HttpNotificationReceived += notificationChannel_HttpNotificationReceived;
+                notificationChannel.ShellToastNotificationReceived += notificationChannel_ShellToastNotificationReceived;
+                notificationChannel.ConnectionStatusChanged+=notificationChannel_ConnectionStatusChanged;
+
+                notificationChannel.Open();
+                BindToShell();
+            }
+        }
+
+        private void notificationChannel_ConnectionStatusChanged(object sender, NotificationChannelConnectionEventArgs e)
+        {
+            Debug.WriteLine("Channel connection status changed to " + e.ConnectionStatus.ToString());
+        }
+
+        private void BindToShell()
+        {
+            try
+            {
+                if (!notificationChannel.IsShellTileBound)
+                {
+                    notificationChannel.BindToShellTile();
+                }
+                if (!notificationChannel.IsShellToastBound)
+                {
+                    notificationChannel.BindToShellToast();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception occured binding to shell: " + e.Message);
+            }
+        }
+
+        private void notificationChannel_ShellToastNotificationReceived(object sender, NotificationEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                tbxInfo.Text = "Toast notification received: ";
+                tbxInfo.Text += "\nCollection: ";
+                foreach (var item in e.Collection.Keys)
+                {
+                    tbxInfo.Text += string.Format("\nKey: {0}, Value: {1}\n", item, e.Collection[item]);
+                }
+            });
+        }
+
+        private void notificationChannel_HttpNotificationReceived(object sender, HttpNotificationEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                tbxInfo.Text = string.Format("Http notification received: \nBody: {0}\nChannel: {1}", e.Notification.Body, e.Notification.Channel);
+                tbxInfo.Text += "\nHeaders: ";
+                foreach (var header in e.Notification.Headers)
+                {
+                    tbxInfo.Text += "\n" + header.ToString();
+                }
+            });
+        }
+
+        private void notificationChannel_ErrorOccurred(object sender, NotificationChannelErrorEventArgs e)
+        {
+            Debug.WriteLine("Error occured: " + e.Message);
+        }
+
+        private void notificationChannel_ChannelUriUpdated(object sender, NotificationChannelUriEventArgs e)
+        {
+            ChannelUri = e.ChannelUri;
         }
 
         // Sample code for building a localized ApplicationBar
--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,77 @@ namespace Server
         public MainWindow()
         {
             InitializeComponent();
+            InitializeParams();
         }
+
+        private void InitializeParams()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            builder.Append("<wp:Notification xmlns:wp=\"WPNotification\">");
+            builder.Append("      <wp:Tile>");
+            builder.Append("            <wp:BackgroundImage>http://hattrickunited.org/cyclonesfc/files/2011/07/boobs1.jpg</wp:BackgroundImage>");
+            builder.Append("            <wp:Count>{0}</wp:Count>");
+            builder.Append("            <wp:Title>{1}</wp:Title>");
+            builder.Append("      </wp:Tile>");
+            builder.Append("</wp:Notification>");
+
+            tilePushXml = builder.ToString();
+
+            builder.Clear();
+            builder.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            builder.Append("<wp:Notification xmlns:wp=\"WPNotification\">");
+            builder.Append("      <wp:Toast>");            
+            builder.Append("            <wp:Text1>{0}</wp:Text1>");
+            builder.Append("            <wp:Text2>{1}</wp:Text2>");
+            builder.Append("            <wp:Param>?developer=Shokhrukh Umriyaev</wp:Param>");
+            builder.Append("      </wp:Toast>");
+            builder.Append("</wp:Notification>");
+
+            toastPushXml = builder.ToString();
+        }
+
+        string toastPushXml = string.Empty;
+        string tilePushXml = string.Empty;
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(tbxUrl.Text))
+            {
+                MessageBox.Show("Please, enter url");
+                return;
+            }
+            if (string.IsNullOrEmpty(tbxText.Text) || string.IsNullOrEmpty(tbxTitle.Text))
+            {
+                MessageBox.Show("Please, fill all the fields");
+                return;
+            }
+
+            string url = tbxUrl.Text;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.Method = "POST";
+            request.Headers = new WebHeaderCollection();
+            request.ContentType = "text/xml";
+
+            request.Headers.Add("X-WindowsPhone-Target", "token");
+            request.Headers.Add("X-NotificationClass", "1");
+
+            string str = string.Format(tilePushXml, tbxTitle.Text, tbxText.Text);
+            byte[] strBytes = Encoding.Default.GetBytes(str);
+            request.ContentLength = strBytes.Length;
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(strBytes, 0, strBytes.Length);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string notificationStatus = response.Headers["X-NotificationStatus"];
+            string deviceConnectionStatus = response.Headers["X-DeviceConnectionStatus"];
+            string subscriptionStatus = response.Headers["X-SubscriptionStatus"];
+            lblStatus.Text = "Notification status: " + notificationStatus + "; Device connection status : " + deviceConnectionStatus + "; Subscription status: " + subscriptionStatus;
+        }
+
+
     }
 }
